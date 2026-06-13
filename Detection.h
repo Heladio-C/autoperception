496ccba5e80aba19bfc7ef9ccfc94bd3bb643968
irregular-user <heladio522@gmail.com>
2026-06-13 01:33:44 -0700

    Add Detection struct to represent object detections with position and type

---
+++ Detection.h
@@ -0,0 +1,14 @@
+#ifndef DETECTION_H
+#define DETECTION_H
+
+#include <string> 
+#include "Vec2.h"
+
+struct Detection{
+
+    Vec2 position;
+    std::string type;
+
+};
+
+#endif
--- Lidar.h
+++ Lidar.h
@@ -7,12 +7,12 @@ class Lidar : public Sensor{
     public:
     Lidar(const std::string& name, const Vec2& position, double range);
 
-    void scan() const override;
+    void scan() const override; // overrides pure virtual function
 
     double getRange() const;
 
     private:
-    double range;
+    double range; // max range in meters
 };
 
 #endif
