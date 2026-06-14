e99ff98d6cdd70a2c10f773e7f3515d2401ccb3a
irregular-user <heladio522@gmail.com>
2026-06-14 16:43:37 -0700

    Add range validation in Lidar constructor to ensure positive values

--- Lidar.cpp
+++ Lidar.cpp
@@ -1,8 +1,15 @@
 #include "Lidar.h"
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 
-Lidar::Lidar(const string& name, const Vec2& position, double range) : Sensor(name, position), range(range) {}
+Lidar::Lidar(const string& name, const Vec2& position, double range) : Sensor(name, position), range(range) {
+
+    if(range <= 0.0){
+        throw invalid_argument("Lidar range must be positive (got" + to_string(range) + ")");
+    }
+
+}
 
 
 double Lidar::getRange() const {
