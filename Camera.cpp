fd27415346a6e880c3f31327bcb343e131465149
irregular-user <heladio522@gmail.com>
2026-06-14 16:43:29 -0700

    Add validation for field of view in Camera constructor

--- Camera.cpp
+++ Camera.cpp
@@ -1,9 +1,14 @@
 #include "Camera.h"
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 
 Camera::Camera(const string& name, const Vec2& position, double fieldOfView)
-    : Sensor(name, position), fieldOfView(fieldOfView) {}
+    : Sensor(name, position), fieldOfView(fieldOfView) {
+        if(fieldOfView <= 0.0 || fieldOfView > 360.0){
+            throw invalid_argument("Field of View must be in (0, 360] degrees");
+        }
+    }
 
 double Camera::getFieldOfView() const { return fieldOfView; }
 
