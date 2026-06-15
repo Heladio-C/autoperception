da99facf5cd1fc1abd47de823c0d1f21e72d2f08
irregular-user <heladio522@gmail.com>
2026-06-14 17:08:31 -0700

    Add validation for max speed in Radar constructor to ensure positive values

--- Radar.cpp
+++ Radar.cpp
@@ -1,8 +1,14 @@
 #include "Radar.h"
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 
-Radar::Radar(const string& name, const Vec2& position, double maxSpeed) : Sensor(name, position), maxSpeed(maxSpeed){}
+Radar::Radar(const string& name, const Vec2& position, double maxSpeed) : Sensor(name, position), maxSpeed(maxSpeed){
+
+    if(maxSpeed <= 0.0){
+        throw invalid_argument("Radar max speed must be positive");
+    }
+}
 
 double Radar::getMaxSpeed() const{return maxSpeed;}
 
