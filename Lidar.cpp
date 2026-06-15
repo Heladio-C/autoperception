37bb4eb663f7933a5a97f4cc5161e8fed4deaf3f
irregular-user <heladio522@gmail.com>
2026-06-14 17:08:15 -0700

    Fix formatting in Lidar range validation error message

--- Lidar.cpp
+++ Lidar.cpp
@@ -6,7 +6,7 @@ using namespace std;
 Lidar::Lidar(const string& name, const Vec2& position, double range) : Sensor(name, position), range(range) {
 
     if(range <= 0.0){
-        throw invalid_argument("Lidar range must be positive (got" + to_string(range) + ")");
+        throw invalid_argument("Lidar range must be positive (got " + to_string(range) + ")");
     }
 
 }
